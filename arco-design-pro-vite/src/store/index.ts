c1f34632af2ff99dd91f825512ec1576f9c24bac
sHow8e <4963222@qq.com>
2022-01-18 16:19:54 +0800

    fix: remove logger package

--- arco-design-pro-vite/src/store/index.ts
+++ arco-design-pro-vite/src/store/index.ts
@@ -1,20 +1,8 @@
 import { createPinia } from 'pinia';
-import { PiniaLogger } from 'pinia-logger';
-import { debug } from '@/utils/env';
 import { useAppStore } from './modules/app';
 import { useUserStore } from './modules/user';
 
 const pinia = createPinia();
-
-if (debug) {
-  pinia.use(
-    PiniaLogger({
-      showStoreName: true,
-      expanded: false,
-      disabled: false,
-    })
-  );
-}
 
 export { useAppStore, useUserStore };
 export default pinia;
