c71c1a01bff8f68dc03d53c089081533e08545cb
sHow8e <4963222@qq.com>
2022-02-08 15:03:26 +0800

    fix: delete login verification

--- arco-design-pro-vite/src/mock/user.ts
+++ arco-design-pro-vite/src/mock/user.ts
@@ -5,7 +5,6 @@ import setupMock, {
 } from '@/utils/setup-mock';
 
 import { MockParams } from '@/types/mock';
-import { isLogin } from '@/utils/auth';
 
 setupMock({
   setup() {
@@ -13,29 +12,26 @@ setupMock({
 
     // 用户信息
     Mock.mock(new RegExp('/api/user/info'), () => {
-      if (isLogin()) {
-        const role = window.localStorage.getItem('userRole') || 'admin';
-        return successResponseWrap({
-          name: '王立群',
-          avatar:
-            '//lf1-xgcdn-tos.pstatp.com/obj/vcloud/vadmin/start.8e0e4855ee346a46ccff8ff3e24db27b.png',
-          email: 'wangliqun@email.com',
-          job: 'frontend',
-          jobName: '前端艺术家',
-          organization: 'Frontend',
-          organizationName: '前端',
-          location: 'beijing',
-          locationName: '北京',
-          introduction: '人潇洒，性温存',
-          personalWebsite: 'https://www.arco.design',
-          phone: '150****0000',
-          registrationDate: '2013-05-10 12:10:00',
-          accountId: '15012312300',
-          certification: 1,
-          role,
-        });
-      }
-      return failResponseWrap(null, 50008, '未登录');
+      const role = window.localStorage.getItem('userRole') || 'admin';
+      return successResponseWrap({
+        name: '王立群',
+        avatar:
+          '//lf1-xgcdn-tos.pstatp.com/obj/vcloud/vadmin/start.8e0e4855ee346a46ccff8ff3e24db27b.png',
+        email: 'wangliqun@email.com',
+        job: 'frontend',
+        jobName: '前端艺术家',
+        organization: 'Frontend',
+        organizationName: '前端',
+        location: 'beijing',
+        locationName: '北京',
+        introduction: '人潇洒，性温存',
+        personalWebsite: 'https://www.arco.design',
+        phone: '150****0000',
+        registrationDate: '2013-05-10 12:10:00',
+        accountId: '15012312300',
+        certification: 1,
+        role,
+      });
     });
 
     // 登录
