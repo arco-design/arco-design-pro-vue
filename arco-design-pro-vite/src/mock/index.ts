5295fc1ca4aa60f52fceb105b34711a611350b49
sHow8e <4963222@qq.com>
2022-02-17 19:50:41 +0800

    feat: simple mode placeholder

--- arco-design-pro-vite/src/mock/index.ts
+++ arco-design-pro-vite/src/mock/index.ts
@@ -4,6 +4,7 @@ import './user';
 import './message-box';
 
 import '@/views/dashboard/workplace/mock';
+/** simple */
 import '@/views/dashboard/monitor/mock';
 
 import '@/views/list/card/mock';
@@ -18,6 +19,7 @@ import '@/views/visualization/multi-dimension-data-analysis/mock';
 
 import '@/views/user/info/mock';
 import '@/views/user/setting/mock';
+/** simple end */
 
 Mock.setup({
   timeout: '600-1000',
