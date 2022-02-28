5945f3cf3eb80f92b1a7071417322f6af10cab15
sHow8e <4963222@qq.com>
2022-02-28 14:57:55 +0800

    chore: use vite merge config

--- arco-design-pro-vite/config/vite.config.dev.ts
+++ arco-design-pro-vite/config/vite.config.dev.ts
@@ -1,50 +1,23 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import svgLoader from 'vite-svg-loader';
+import { mergeConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import baseConig from './vite.config.base';
 
-export default defineConfig({
-  mode: 'development',
-  server: {
-    open: true,
-    fs: {
-      strict: true,
+export default mergeConfig(
+  {
+    mode: 'development',
+    server: {
+      open: true,
+      fs: {
+        strict: true,
+      },
     },
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    svgLoader({ svgoConfig: {} }),
-    eslint({
-      cache: false,
-      include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
-      exclude: ['node_modules'],
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: resolve(__dirname, '../src'),
-      },
-      {
-        find: 'assets',
-        replacement: resolve(__dirname, '../src/assets'),
-      },
-      {
-        find: 'vue-i18n',
-        replacement: 'vue-i18n/dist/vue-i18n.cjs.js', // Resolve the i18n warning issue
-      },
-      {
-        find: 'vue',
-        replacement: 'vue/dist/vue.esm-bundler.js', // compile template
-      },
+    plugins: [
+      eslint({
+        cache: false,
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+        exclude: ['node_modules'],
+      }),
     ],
-    extensions: ['.ts', '.js'],
   },
-  define: {
-    'process.env': {},
-  },
-});
+  baseConig
+);
--- arco-design-pro-vite/config/vite.config.prod.ts
+++ arco-design-pro-vite/config/vite.config.prod.ts
@@ -1,34 +1,9 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import svgLoader from 'vite-svg-loader';
+import { mergeConfig } from 'vite';
+import baseConig from './vite.config.base';
 
-export default defineConfig({
-  mode: 'production',
-  plugins: [vue(), vueJsx(), svgLoader({ svgoConfig: {} })],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: resolve(__dirname, '../src'),
-      },
-      {
-        find: 'assets',
-        replacement: resolve(__dirname, '../src/assets'),
-      },
-      {
-        find: 'vue-i18n',
-        replacement: 'vue-i18n/dist/vue-i18n.cjs.js', // Resolve the i18n warning issue
-      },
-      {
-        find: 'vue',
-        replacement: 'vue/dist/vue.esm-bundler.js', // compile template. you can remove it, if you don't need.
-      },
-    ],
-    extensions: ['.ts', '.js'],
+export default mergeConfig(
+  {
+    mode: 'production',
   },
-  define: {
-    'process.env': {},
-  },
-});
+  baseConig
+);
