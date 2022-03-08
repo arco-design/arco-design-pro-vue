a4e69de37a0f4d1c689b33f1be29d62b97860f57
sHow8e <4963222@qq.com>
2022-03-08 11:39:26 +0800

    feat: improve the router change performance

--- arco-design-pro-vite/src/router/index.ts
+++ arco-design-pro-vite/src/router/index.ts
@@ -1,13 +1,11 @@
-import { createRouter, createWebHistory, LocationQueryRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import NProgress from 'nprogress'; // progress bar
 import 'nprogress/nprogress.css';
 
-import usePermission from '@/hooks/permission';
-import { useUserStore } from '@/store';
 import PageLayout from '@/layout/page-layout.vue';
-import { isLogin } from '@/utils/auth';
 import Login from './modules/login';
 import appRoutes from './modules';
+import createRouteGuard from './guard';
 
 NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
@@ -36,56 +34,6 @@ const router = createRouter({
   },
 });
 
-router.beforeEach(async (to, from, next) => {
-  NProgress.start();
-  const userStore = useUserStore();
-  async function crossroads() {
-    const Permission = usePermission();
-    if (Permission.accessRouter(to)) await next();
-    else {
-      const destination = Permission.findFirstPermissionRoute(
-        appRoutes,
-        userStore.role
-      ) || {
-        name: 'notFound',
-      };
-      await next(destination);
-    }
-    NProgress.done();
-  }
-  if (isLogin()) {
-    if (userStore.role) {
-      crossroads();
-    } else {
-      try {
-        await userStore.info();
-        crossroads();
-      } catch (error) {
-        next({
-          name: 'login',
-          query: {
-            redirect: to.name,
-            ...to.query,
-          } as LocationQueryRaw,
-        });
-        NProgress.done();
-      }
-    }
-  } else {
-    if (to.name === 'login') {
-      next();
-      NProgress.done();
-      return;
-    }
-    next({
-      name: 'login',
-      query: {
-        redirect: to.name,
-        ...to.query,
-      } as LocationQueryRaw,
-    });
-    NProgress.done();
-  }
-});
+createRouteGuard(router);
 
 export default router;
--- arco-design-pro-vite/src/router/typings.d.ts
+++ arco-design-pro-vite/src/router/typings.d.ts
@@ -2,14 +2,13 @@ import 'vue-router';
 
 declare module 'vue-router' {
   interface RouteMeta {
-    // options
-    roles?: string[];
-    // every route must declare
-    requiresAuth: boolean; // need login
-    icon?: string;
-    locale?: string;
-    // menu select key
-    menuSelectKey?: string;
-    hideInMenu?: boolean;
+    roles?: string[]; // Controls roles that have access to the page
+    requiresAuth: boolean; // Whether login is required to access the current page (every route must declare)
+    icon?: string; // The icon show in the side menu
+    locale?: string; // The locale name show in side menu and breadcrumb
+    activeMenu?: string; // if set key, the side menu will highlight you settings when a path is matched
+    hideInMenu?: boolean; // If true, it is not displayed in the side menu
+    affix?: true; // if set true, the tag will affix in the tab-bar
+    tagIcon?: string; // if set key, the icon will show in the tab-bar
   }
 }
