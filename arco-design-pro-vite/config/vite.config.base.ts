5a643302b379d2c605a4973b5652f2736983f71a
sHow8e <4963222@qq.com>
2022-03-09 14:41:17 +0800

    chore: build config

--- arco-design-pro-vite/config/vite.config.base.ts
+++ arco-design-pro-vite/config/vite.config.base.ts
@@ -3,9 +3,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
+import { injectHtml } from 'vite-plugin-html';
+import { teaScript, teaRun } from '../../../inject-script';
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), svgLoader({ svgoConfig: {} })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    svgLoader({ svgoConfig: {} }),
+    injectHtml({
+      data: {
+        teaScript,
+        teaRun,
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
