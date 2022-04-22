b548eaa44aff6d1da70742d5b5dd44fc7c8ae94d
hehehai <riverhohai@gmail.com>
2022-04-22 21:28:46 +0800

    fix: nested route cache

--- arco-design-pro-vite/src/layout/page-layout.vue
+++ arco-design-pro-vite/src/layout/page-layout.vue
@@ -1,19 +1,10 @@
 <template>
-  <router-view>
-    <template #default>
-      <router-view v-slot="{ Component, route }">
-        <transition name="fade" mode="out-in" appear>
-          <component
-            :is="Component"
-            v-if="route.meta.ignoreCache"
-            :key="route.fullPath"
-          />
-          <keep-alive v-else :include="cacheList">
-            <component :is="Component" :key="route.fullPath" />
-          </keep-alive>
-        </transition>
-      </router-view>
-    </template>
+  <router-view v-slot="{ Component, route }">
+    <transition name="fade" mode="out-in" appear>
+      <keep-alive :include="cacheList">
+        <component :is="Component" :key="route.fullPath" />
+      </keep-alive>
+    </transition>
   </router-view>
 </template>
 
