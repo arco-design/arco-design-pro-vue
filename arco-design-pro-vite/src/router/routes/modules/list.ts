7a8e2d999f74d7d4a8fa45cbed95646121521b86
安一老厨 <62740686+zxiaozhou@users.noreply.github.com>
2023-02-03 18:23:51 +0800

    恢复被改动到的原生list文件
    恢复被改动到的原生list文件

--- arco-design-pro-vite/src/router/routes/modules/list.ts
+++ arco-design-pro-vite/src/router/routes/modules/list.ts
@@ -6,7 +6,7 @@ const LIST: AppRouteRecordRaw = {
   name: 'list',
   component: DEFAULT_LAYOUT,
   meta: {
-    locale: 'mutilMenu.list',
+    locale: 'menu.list',
     requiresAuth: true,
     icon: 'icon-list',
     order: 2,
